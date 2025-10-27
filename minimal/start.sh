9023661ad32af5140210b8b7c7ac2b4ddc1b5dad
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-27 12:49:50 +0000

    Update start.sh

--- minimal/start.sh
+++ minimal/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 nohup /usr/bin/plasmashell > /dev/null 2>&1 &
 sleep 1
-plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/SpitFire/spitfire.png > /dev/null 2>&1
-plasma-apply-colorscheme SpitFire > /dev/null 2>&1
-sudo rm -rf /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge
+plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/Apex/rift.jpg > /dev/null 2>&1
+plasma-apply-colorscheme Apex > /dev/null 2>&1
+sudo rm -rf /home/$USER/vanillaarch-or-cachyos-to-claudemods-apex-ckge
 sudo rm -rf /home/$USER/appimages.zip
 sudo rm -rf /home/$USER/apps
