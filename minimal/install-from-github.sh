e535f0b4c74deb83646f2044fe3ece9a144793d1
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-27 13:09:09 +0000

    fixed for apex

--- minimal/install-from-github.sh
+++ minimal/install-from-github.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-or-cachyos-to-claudemods-apex-ckge && cd /home/$USER/vanillaarch-or-cachyos-to-claudemods-apex-ckge/kdegrub-minimal && sudo chmod +x /home/$USER/vanillaarch-or-cachyos-to-claudemods-apex-ckge/kdegrub-minimal/install-repo-and-full-cachyos-kde-grub-packages.sh && ./install-repo-and-full-cachyos-kde-grub-packages.sh
+cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-or-cachyos-to-claudemods-apex-ckge && cd /home/$USER/vanillaarch-or-cachyos-to-claudemods-apex-ckge/minimal && sudo chmod +x /home/$USER/vanillaarch-or-cachyos-to-claudemods-apex-ckge/minimal/install-repo-and-full-cachyos-kde-grub-packages.sh && ./install-repo-and-full-cachyos-kde-grub-packages.sh
