801df7c1ce5b7a45d798fa51ea775d6967d03d83
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-04 22:25:57 +0000

    Update start.sh

--- minimal/start.sh
+++ minimal/start.sh
@@ -4,6 +4,7 @@ nohup /usr/bin/plasmashell > /dev/null 2>&1 &
 sleep 1
 plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/Apex/rift.jpg > /dev/null 2>&1
 plasma-apply-colorscheme Apex > /dev/null 2>&1
+cp -r /home/$USER/vanillaarch-or-cachyos-to-claudemods-apex-ckge/minimal/konsolerc /home/$USER/.config
 sudo rm -rf /home/$USER/vanillaarch-or-cachyos-to-claudemods-apex-ckge
 sudo rm -rf /home/$USER/appimages.zip
 sudo rm -rf /home/$USER/apps
