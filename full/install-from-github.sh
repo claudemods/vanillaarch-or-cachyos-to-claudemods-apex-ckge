1447cc6fe1f56a35f1f865fd6e7d02e45bc80b0d
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-16 12:00:45 +0000

    Create install-from-github.sh

---
+++ full/install-from-github.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-or-cachyos-to-claudemods-apex-ckge && cd /home/$USER/vanillaarch-or-cachyos-to-claudemods-apex-ckge/full && sudo chmod +x /home/$USER/vanillaarch-or-cachyos-to-claudemods-apex-ckge/full/install-apex-ckge-minimal-updated.sh && ./install-apex-ckge-minimal-updated.sh
